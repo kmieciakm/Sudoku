351b11905e7079e7d5bddfc9f241cad00fdd5993
224092 Kmieciak Mateusz <224092@edu.p.lodz.pl>
2020-04-01 14:57:31 +0200

    Small clean-up, deleted unused packages imports

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 interface SudokuSolver {
-    public void solve(SudokuBoard board);
+    void solve(SudokuBoard board);
 }
